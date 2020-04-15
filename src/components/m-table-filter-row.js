f6cc6b5fdd98157e976d5e1a8f4041e8bc862465
Chris533 <eskim0@qq.com>
2020-04-15 21:49:38 +0800

    Fix icons with FilterIcon

--- src/components/m-table-filter-row.js
+++ src/components/m-table-filter-row.js
@@ -78,6 +78,7 @@ class MTableFilterRow extends React.Component {
 
   renderDefaultFilter = (columnDef) => {
     const localization = { ...MTableFilterRow.defaultProps.localization, ...this.props.localization };
+    const FilterIcon = this.props.icons.Filter
     return (
       <TextField
         style={columnDef.type === 'numeric' ? { float: 'right' } : {}}
@@ -91,7 +92,7 @@ class MTableFilterRow extends React.Component {
           startAdornment: (
             <InputAdornment position="start">
               <Tooltip title={localization.filterTooltip}>
-                <this.props.icons.Filter />
+                <FilterIcon />
               </Tooltip>
             </InputAdornment>
           )
@@ -138,9 +139,6 @@ class MTableFilterRow extends React.Component {
   }
 
   getComponentForColumn(columnDef) {
-    if (!columnDef.icons) {
-      return null
-    }
     if (columnDef.filtering === false) {
       return null;
     }
